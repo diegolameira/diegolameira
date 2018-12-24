cb6aa0e8ca4a1e7dbaa69048000081182cfab79b
Diego Lameira <diego.lameira@dextra-sw.com>
2018-12-24 18:50:25 -0200

    add some style and ignore build

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 import ProfilePage from './profile';
 
+import './styles.css';
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(<ProfilePage />, rootElement);
